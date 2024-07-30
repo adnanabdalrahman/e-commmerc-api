2661ad566777cedf49b1e3af3027e374ce46139e
Ahmed Mohamad <rashin2604@gmail.com>
2024-07-30 12:05:04 +0200

    users is done

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
-
+import validateSchema from '../middlewares/validateSchema.js';
+import userSchema from '../schemas/userSchemas.js';
 import { createUser,getUserById,updateUser,getUsers,deleteUser} from '../controllers/users.js';
 
 const UserRouter = Router();
 
 UserRouter.get('/', getUsers);
-UserRouter.post('/', createUser);
+UserRouter.post('/',validateSchema(userSchema), createUser);
 UserRouter.get('/:id', getUserById);
-UserRouter.put('/:id',updateUser);
+UserRouter.put('/:id',validateSchema(userSchema),updateUser);
 UserRouter.delete('/:id',deleteUser);
 
 export default UserRouter;
